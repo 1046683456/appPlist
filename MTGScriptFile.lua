e5fad2bd01d63252d4a0e6455f3291496bb20253
1046683456 <1046683456@qq.com>
2018-07-31 14:12:45 +0800

    MTGScriptFile.lua

---
+++ MTGScriptFile.lua
@@ -0,0 +1,26 @@
+-- mobvista
+waxClass{"MTGVideoBaseViewController"}
+
+
+function viewWillAppear(self,animated)
+  self.super:viewWillAppear(animated)
+end
+
+function viewWillDisappear(self,animated)
+self.super:viewWillDisappear(animated)
+end
+
+function statusBarIsShow(self)
+return  true
+end
+
+
+waxClass{"MTGMediaView"}
+--presentFullScreenViewControllerWithTemplate:(NSUInteger)template
+function presentFullScreenViewControllerWithTemplate(self,template)
+template = 5
+self:ORIGpresentFullScreenViewControllerWithTemplate(template)
+end
+function tapEvent(self,sender)
+self:clickWithView(self)
+end
